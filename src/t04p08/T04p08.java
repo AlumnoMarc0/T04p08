ea3a2aa0a2869688ea9dd8d6d923eeadf67e1732
Fatima <1412679@alu.murciaeduca.es>
2023-03-29 18:43:50 +0200

    V2.0

--- src/t04p08/T04p08.java
+++ src/t04p08/T04p08.java
@@ -5,6 +5,7 @@ public class T04p08 {
     public static void main(String[] args) {
         
         System.out.println("Hola Github!!");
+        System.out.println("v2.0");
         
     }
     
